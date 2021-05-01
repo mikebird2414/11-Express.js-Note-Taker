971eee916ab8ae4b2af3cc7ba2da63ca79aced3e
Mikebird2414 <Mikebird2414@yahoo.com>
2021-05-01 10:43:06 -0400

    server

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-// Dependencies
-// =============================================================
-const express = require("express");
-const fs = require("fs");
+// DEPENDENCIES
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080
+const express = require('express');
 
-// Sets up the Express app to handle data parsing
-// =============================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+
+// Tells node that we are creating an "express" server
+const app = express();
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/assets", express.static("./assets"));
+app.use(express.static('public'));
 
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
-require("./route/html-route")(app);
-require("./route/api-route")(app);
+require('./route/api-routes')(app);
+require('./route/html-routes')(app);
 
-// Starts the server to begin listening
-// =============================================================
+// LISTENER
+// The below code effectively starts our server
+
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+  console.log(`Server is listening on PORT: ${PORT}`);
 });
